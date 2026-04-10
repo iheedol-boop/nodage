e619a141e813d12eb03fd63d4af3258d4698d00f
iheedol-boop <iheedol@gmail.com>
2026-04-10 16:10:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ if run_analysis:
                 name_match = all_listing[all_listing['Code'] == code]['Name']
                 name = name_match.values[0] if not name_match.empty else "미등록"
                 
-                if code='411060' :
+                if code=='411060' :
                     current_price=current_price*7.15
                     prev_close=prev_close*7.15
 
