80bf48d57fe03b0375d63891879267f2197c9b72
iheedol-boop <iheedol@gmail.com>
2026-04-13 09:24:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import FinanceDataReader as fdr
 import pandas as pd
 import plotly.express as px
-from streamlit_gsheets import GSheetsConnection
+from st_gsheets_connection import GSheetsConnection
 
 # ------------------- 설정 -------------------
 st.set_page_config(page_title="자산 관리 (G-Sheet)", layout="wide")
