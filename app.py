58857ad535444946c5be738bbdcaaaae397af38a
iheedol-boop <iheedol@gmail.com>
2026-04-17 16:37:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from dotenv import load_dotenv
 
 st.markdown("""
     <style>
+    /* 서브해더(타이틀) 크기 조절 */
+    [data-testid="expander"] {
+        font-size: 1.1rem !important;
+        font-weight: 700;
+    }
     /* 라벨(종목명) 크기 조절 */
     [data-testid="stMetricLabel"] {
         font-size: 0.8rem !important;
