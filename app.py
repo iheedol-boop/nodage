06777059e8b32bd939ca90694a009227e122d6f2
iheedol-boop <iheedol@gmail.com>
2026-04-30 13:29:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -123,20 +123,20 @@ def get_stock_list():
 all_listing = get_stock_list()
 
 # ====================== 조회 섹션 ======================
-with st.expander("💳 1. 계좌 정보 조회", expanded=False): # 보기 편하도록 기본 접음
+with st.expander("💳 1. 원금 및 예수금", expanded=False): # 보기 편하도록 기본 접음
     df_acc = load_accounts()
     st.dataframe(
         df_acc,
         hide_index=True  # 인덱스 번호를 숨기면 더 깔끔합니다
     )
 
-with st.expander("📈 2. 보유 종목 조회", expanded=False):
+with st.expander("📈 2. 보유 수량", expanded=False):
     df_stock = load_holdings()
     st.dataframe(
         df_stock,
         hide_index=True
     )
-with st.expander("🚀 3. 보유 예금 조회", expanded=False):
+with st.expander("🏦 3. 예금", expanded=False):
     df_deposit = load_deposit()
     st.dataframe(
         df_deposit,
